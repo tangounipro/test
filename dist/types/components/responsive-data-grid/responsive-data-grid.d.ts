b6604ba7cf6f520a11e2eafb2490d808119e560c
Tan Kent <Tan Kent@Unipro2.local>
2021-05-13 06:28:41 +0100

    up

---
+++ dist/types/components/responsive-data-grid/responsive-data-grid.d.ts
@@ -0,0 +1,12 @@
+export declare class ResponsiveDataGrid {
+  first: string;
+  maxcolumns: string;
+  jsonfilepath: string;
+  titlefontsize: string;
+  descriptionfontsize: string;
+  cardtitlebg: string;
+  productList: any;
+  componentWillRender(): Promise<void>;
+  private checkColCount;
+  render(): any;
+}
